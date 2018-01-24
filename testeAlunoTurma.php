b3daa29d22c8148fc91a8c2d340f03bbfaa382fc
php 19 <php.19@catatau.cec.ime.usp.br>
2018-01-24 21:05:11 -0200

    corrigindo data

--- testeAlunoTurma.php
+++ testeAlunoTurma.php
@@ -7,7 +7,7 @@
 
   $aluno->nome = 'Rafael Cosentino';
   $aluno->rg = '11111111';
-  $aluno->dataNascimento = '30/10/1994';
+  $aluno->dataNascimento = new DateTime('1994-10-30');
   $aluno->turma->periodo = 'Manha';
   $aluno->turma->serie = '5';
   $aluno->turma->sigla = 'B';
--- testeClienteCartaoDeCredito.php
+++ testeClienteCartaoDeCredito.php
@@ -5,7 +5,7 @@
   $cartao = new CartaoDeCredito();
 
   $cartao->numero = 111111;
-  $cartao->dataDeValidade = DateTime::createFromFormat('d-m-Y', '12/12/2018');
+  $cartao->dataDeValidade = new DateTime('2018-12-12');
   $cartao->cliente->nome = 'Rafael Cosentino';
   $cartao->cliente->codigo = 123;
 
